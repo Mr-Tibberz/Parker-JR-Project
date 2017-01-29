0690e54995bed334070247994ff22db57f3f8a51
Allen C. Gardei <gardeia@ferris.edu>
2017-01-29 18:13:18 -0500

    Animation Testing
    This build is the playground for testing animations with the old assets within unity as a proof of concept. Camera tests were disabled in this update, but still present.

---
+++ Parker_DAGD_340/Assets/DetectEndAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectEndAnim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void animEnded()
+    {
+        GetComponent<Animator>().SetBool("IsIdle", true);
+        print("triggered");
+    }
+}
---
+++ Parker_DAGD_340/Assets/WorldController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    public Animator machine_1;
+    AnimationEvent ae = new AnimationEvent(); 
+
+	// Use this for initialization
+	void Start () {
+        machine_1.SetBool("IsIdle", true);
+        ae.messageOptions = SendMessageOptions.DontRequireReceiver;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            machine_1.SetBool("IsIdle", false);
+            print("test");        
+        }
+        
+	}
+
+    public void animEnded()
+    {
+        machine_1.SetBool("IsIdle", true);
+    }
+}
