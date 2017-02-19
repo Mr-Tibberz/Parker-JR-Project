6e305b8027b3b37312da9e15982c1e04bfb510f4
Allen C. Gardei <gardeia@ferris.edu>
2017-02-19 18:25:51 -0500

    Sprint 2 Part 1 Submit
    Camera Will Transition between 9 preset positions, either by using numpad or basic UI buttons. Fixed

--- Parker_DAGD_340/Assets/Gardei_CamPan.cs
+++ Parker_DAGD_340/Assets/Gardei_CamPan.cs
@@ -18,35 +18,40 @@ public class Gardei_CamPan : MonoBehaviour {
      
      */
 
-
-
-    //float t = currentLerpTime / lerpTime;
-    //t = Mathf.Sin(t* Mathf.PI * 0.5f);
-
-
     /// <summary>
-    /// Transform: camPos1 Target lerp position for station 1.
+    /// camPos1: Target lerp position for station 1.
+    /// camPos2: Target lerp position for station 2.
+    /// camPos3: Target lerp position for station 3.
+    /// camPos4: Target lerp position for station 4.
+    /// camPos5: Target lerp position for station 5.
+    /// camPos6: Target lerp position for station 6.
+    /// camPos7: Target lerp position for station 7.
+    /// camPosSky: Target lerp position for Skyview.
+    /// camPosHMI: Target lerp position for HMI.
+    /// priorPos: position of camera on last frame
+    /// priorRot: rotation of camera on last frame 
+    /// targetPos: position of target destination for camera
+    /// targetRot: rotation of target rotation for camera
+    /// moveCam: yes/no is the camera transitioning to a new position.
+    /// lerpTime: maximum value of a lerp sequence.
+    /// currentLerpTime: value between 0 and 1 will guide the camera through a transition to a new location.
     /// </summary>
+
     public GameObject camPos1; //set in editor please
     public GameObject camPos2; //set in editor please
     public GameObject camPos3; //set in editor please
-
-    /// <summary>
-    /// priorPos: position of camera on last frame
-    /// priorRot: rotation of camera on last frame
-    /// 
-    /// targetPos: position of target destination for camera
-    /// targetRot: rotation of target rotation for camera
-    /// </summary>
+    public GameObject camPos4; //set in editor please
+    public GameObject camPos5; //set in editor please
+    public GameObject camPos6; //set in editor please
+    public GameObject camPos7; //set in editor please
+    public GameObject camPosSky; //set in editor please
+    public GameObject camPosHMI; //set in editor please
+    
     Vector3 priorPos;
-    Vector3 priorRot;
-
+    Quaternion priorRot;
     Vector3 targetPos;
-    Vector3 targetRot;
+    Quaternion targetRot;
 
-    /// <summary>
-    /// moveCam: yes/no is the camera transitioning to a new position.
-    /// </summary>
     bool moveCam = false;
 
     float lerpTime = 1f;
@@ -55,10 +60,10 @@ public class Gardei_CamPan : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         priorPos = transform.position;
-        priorRot = transform.rotation.eulerAngles;
+        priorRot = transform.rotation;
 
         targetPos = transform.position;
-        targetRot = transform.rotation.eulerAngles;
+        targetRot = transform.rotation;
 	}
 	
 	// Update is called once per frame
@@ -68,79 +73,82 @@ public class Gardei_CamPan : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Keypad1)) setTarget(1);
         if (Input.GetKeyDown(KeyCode.Keypad2)) setTarget(2);
         if (Input.GetKeyDown(KeyCode.Keypad3)) setTarget(3);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //setTarget(1);
-            
-            /* DEBUG TESTING FOR ROTATION ISSUE
-             * 
-             * Found when trying to set a negative rotation, it automatically goes up to a positive number and thus can never turn left if you have to go from 0 to 360. May need to create a dummy angle value that can go negative to feed into lerp function?
-            print("1: " + priorRot);
-            print("2: " + camPos1.gameObject.transform.eulerAngles);
-
-            print("CamY Rotation: " + camPos1.gameObject.transform.eulerAngles.y);
-            if (camPos1.gameObject.transform.eulerAngles.y >= priorRot.y + 180)
-            {
-                print("OverRot Detected");
-                print("CamY: " + camPos1.gameObject.transform.eulerAngles.y);
-                camPos1.transform.eulerAngles = new Vector3(
-                    camPos1.transform.eulerAngles.x,
-                    camPos1.transform.eulerAngles.y,
-                    camPos1.transform.eulerAngles.z
-                    );
-                print("CamY: " + camPos1.gameObject.transform.eulerAngles.y);
-            }
-            print("CamY Rotation: " + camPos1.gameObject.transform.eulerAngles.z);
-            */
-        }
-        
-        
-            
-            
-            //divide to slow down, or multiply to speed up
-            currentLerpTime += Time.deltaTime / 2;
+        if (Input.GetKeyDown(KeyCode.Keypad4)) setTarget(4);
+        if (Input.GetKeyDown(KeyCode.Keypad5)) setTarget(5);
+        if (Input.GetKeyDown(KeyCode.Keypad6)) setTarget(6);
+        if (Input.GetKeyDown(KeyCode.Keypad7)) setTarget(7);
+        if (Input.GetKeyDown(KeyCode.Keypad8)) setTarget(8);
+        if (Input.GetKeyDown(KeyCode.Keypad9)) setTarget(9);
+        //divide to slow down, or multiply to speed transition up
+        currentLerpTime += Time.deltaTime / 2;
             if (currentLerpTime > lerpTime)
             {
                 currentLerpTime = lerpTime;
             }
 
             float perc = currentLerpTime / lerpTime; //percent 0 - 1
-            perc = perc*perc*perc * (perc * (6f*perc - 15f) + 10f);
-            //SETUP SWITCH HERE TO CONTROL MULTIPLE POINTS.
+            perc = perc*perc*perc * (perc * (6f*perc - 15f) + 10f); //ease in, ease out maths (Creates s curve on a graph)
+
             transform.position = Vector3.Lerp(priorPos, targetPos, perc);
-            transform.eulerAngles = Vector3.Lerp(priorRot, targetRot, perc);
-        
-        
+            transform.rotation = Quaternion.Lerp(priorRot, targetRot, perc);
 	}
-
-    public void panCam() //triggered by UI or SPACE when debugging
-    {
-        
-        currentLerpTime = 0f;
-        //set prior transform values
-        priorPos = transform.position;
-        priorRot = transform.rotation.eulerAngles;
-    }
-
+    /// <summary>
+    /// SetTarget: This function takes a target and will then immediately execute a camera transition as soon as it has been triggered.
+    /// </summary>
+    /// <param name="stationNum">integer that will tell the camera which preset position it needs to go to.</param>
     public void setTarget(int stationNum)
     {
         switch (stationNum)
         {
             case 1:
                 targetPos = camPos1.gameObject.transform.position;
-                targetRot = camPos1.gameObject.transform.eulerAngles;
+                targetRot = camPos1.gameObject.transform.rotation;
                 break;
             case 2:
                 targetPos = camPos2.gameObject.transform.position;
-                targetRot = camPos2.gameObject.transform.eulerAngles;
+                targetRot = camPos2.gameObject.transform.rotation;
                 break;
             case 3:
                 targetPos = camPos3.gameObject.transform.position;
-                targetRot = camPos3.gameObject.transform.eulerAngles;
+                targetRot = camPos3.gameObject.transform.rotation;
+                break;
+            case 4:
+                targetPos = camPos4.gameObject.transform.position;
+                targetRot = camPos4.gameObject.transform.rotation;
+                break;
+            case 5:
+                targetPos = camPos5.gameObject.transform.position;
+                targetRot = camPos5.gameObject.transform.rotation;
+                break;
+            case 6:
+                targetPos = camPos6.gameObject.transform.position;
+                targetRot = camPos6.gameObject.transform.rotation;
+                break;
+            case 7:
+                targetPos = camPos7.gameObject.transform.position;
+                targetRot = camPos7.gameObject.transform.rotation;
+                break;
+            case 8: //SKYVIEW
+                targetPos = camPosSky.gameObject.transform.position;
+                targetRot = camPosSky.gameObject.transform.rotation;
+                break;
+            case 9: //HMI
+                targetPos = camPosHMI.gameObject.transform.position;
+                targetRot = camPosHMI.gameObject.transform.rotation;
                 break;
         }
         panCam();
+    }
+    /// <summary>
+    /// Resets the "lerp" code so the transition will start over from the current position.
+    /// </summary>
+    public void panCam() //triggered by UI or SPACE when debugging
+    {
+
+        currentLerpTime = 0f;
+        //set prior transform values
+        priorPos = transform.position;
+        priorRot = transform.rotation;
     }
 
 }
