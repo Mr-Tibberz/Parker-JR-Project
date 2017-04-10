835fd0fed7001ea8f1630da689abbaa02ea28cbd
Allen C. Gardei <gardeia@ferris.edu>
2017-04-10 06:53:14 -0400

    Updated UI so that selection buttons hover over each station.
    Buttons now float above each station and face the camera. Allowing the user to simply click above the specific station they want to zoom in on.

---
+++ Parker_DAGD_340/Assets/faceCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class faceCam : MonoBehaviour {
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.LookRotation(transform.position - target.position);
+	}
+}
