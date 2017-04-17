69b414914aa48e2a6d627ad1830e2c489aa83cd9
Allen C. Gardei <gardeia@ferris.edu>
2017-04-16 20:37:54 -0400

    FOLLOW MODE!
    Follow mode implmented. user can stop following mid animation and if they choose to follow again it will go to the currently active station. When 1 full routine finishes, cam returns to skyview.

--- Parker_DAGD_340/Assets/DetectEndAnim.cs
+++ Parker_DAGD_340/Assets/DetectEndAnim.cs
@@ -10,6 +10,14 @@ public class DetectEndAnim : MonoBehaviour {
     /// A button passed in from the Unity Editor that you want to reappear when the animation has completed.
     /// </summary>
     public GameObject startAnimBttn = null; //set in editor
+    public GameObject followBttn = null;
+    /// <summary>
+    /// UI Object passed in from Editor so it can be accessed in this script. This value is modified in this script for the follow mode.
+    /// </summary>
+    public Dropdown dropdown;
+    int stationNum = 1;
+    bool follow = false;
+    
 
     public void animEnded()
     {
@@ -17,5 +25,54 @@ public class DetectEndAnim : MonoBehaviour {
         startAnimBttn.GetComponentInChildren<Text>().text = "►";
         //startAnimBttn.SetActive(true);
         print("returning to idle 33");
+        stationNum = 1;
+        dropdown.value = 0;
+        follow = false;
+        followBttn.GetComponentInChildren<Text>().text = "Follow";
+    }
+    //called when animation reaches a certain point on timeline so that the camera better follows the bottole.
+    /// <summary>
+    /// This should be called as an event 
+    /// </summary>
+    public void nextStation()
+    {
+        stationNum++;
+
+        if (follow)
+        {
+            if (stationNum == 0 || stationNum == 8)
+            {
+                stationNum = 1;
+                dropdown.value = 1;
+            }
+            else if (stationNum == 8)
+            {
+                //Return to Sky
+                stationNum = 0;
+                dropdown.value = 0;
+            }
+            else dropdown.value = stationNum;
+
+           
+        }
+        
+    }
+    public void followToggle()
+    {
+        if (!follow)
+        {
+            follow = true;
+            followBttn.GetComponentInChildren<Text>().text = "Unfollow";
+            dropdown.value = stationNum;
+        }
+        else
+        {
+            follow = false;
+            followBttn.GetComponentInChildren<Text>().text = "Follow";
+        }
+    }
+    public void changeDropDown(int ddv)
+    {
+        dropdown.value = ddv;
     }
 }
